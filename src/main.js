13c26f0d4511fe4dd802705fc05c46c4694e8d42
Meena044 <meena290470@gmail.com>
2025-11-15 23:37:01 +0530

    added background and content to legend and few styling changes

---
+++ src/main.js
@@ -0,0 +1,29 @@
+// const themeList = [
+//     "./image/digital.jpg",
+//     "./image/orangeImg.jpg",
+//     "./image/raindowImg.jpg",
+//     "./image/backgroung.jpg"
+// ];
+
+const title = document.querySelector('#title');
+
+// document.body.style.backgroundImage = 'url("./image/backgroung.jpg")';
+
+// let currentTheme=null;
+
+// const themeIcon = document.querySelector('#theme-icon');
+// themeIcon.addEventListener('click', ()=>{
+//     let newTheme;
+
+//     do{
+//         newTheme= themeList[Math.floor(Math.random() * themeList.length)];
+//     }while(newTheme === currentTheme);
+
+//     currentTheme = newTheme;
+
+    // if(newTheme = './image/digital.jpg'){
+    //     title.style.color = "white";
+    // }
+
+//     document.body.style.backgroundImage = `url(${newTheme})`;
+// });
