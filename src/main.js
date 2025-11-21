d5fb5d7ec9305983bababb9814d8623bf43c3974
Meena044 <meena290470@gmail.com>
2025-11-21 23:08:37 +0530

    moved tab switching method to a separate module

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { addingNewProject }  from "./addProject.js";
 import { loadProjectsFromStorage } from "./projectDisplay.js"
 import { adddingNewList } from "./addList.js";
 import { deletelist, deleteProject } from "./delete.js";
+import { highlightBG } from "./tabSwitching.js";
 import { allTab } from "./allTab.js";
 import { inboxTab } from "./inboxTab.js";
 // import { todayTab } from "./todayTab.js";
@@ -18,13 +19,8 @@ const projectdialog = document.getElementById('newProject');
 const listdialog = document.getElementById('newList');
 const UlTagOfProjects = document.getElementById('lists');
 const selectTag = document.getElementById('projectList');
-// const deleteicon = document.querySelector('.deleticon');
-
 const legendOptions = document.getElementById('projects')
-let items = legendOptions.querySelectorAll('li');
-
-const projectsOptions = document.getElementById('lists')
-
+const projectsOptions = document.getElementById('lists');
 const allbtn = document.getElementById('all');
 const inboxbtn = document.getElementById('inbox');
 const todaybtn = document.getElementById('today');
@@ -51,32 +47,15 @@ UlTagOfProjects.addEventListener('click', (e)=> deletelist(e.target));
 
 // tabs in legend background color change
 legendOptions.addEventListener('click', (e)=>{
-     if (e.target.tagName !== "LI") return; // click only on <li>
-
-    // remove background from all tabs
-    items.forEach(item => {
-        item.style.backgroundColor = "";
-        item.style.borderRadius = "";
-    });
-
-    e.target.style.backgroundColor = ' rgba(74, 6, 119, 0.4)';
-    e.target.style.borderRadius = '1.3rem';
+    if (e.target.tagName !== "LI") return; // click only on <li>
+    highlightBG(e.target);
 });
 
 projectsOptions.addEventListener('click', (e)=>{
-     if (e.target.tagName !== "LI") return; // click only on <li>
-
-     const projOpt = projectsOptions.querySelectorAll("li");
-
-    // remove background from all tabs
-    projOpt.forEach(opt => {
-        opt.style.backgroundColor = "";
-        opt.style.borderRadius = "";
-    });
-
-    e.target.style.backgroundColor = ' rgba(74, 6, 119, 0.4)';
-    e.target.style.borderRadius = '1.3rem';
+    if (e.target.tagName !== "LI") return; 
+    highlightBG(e.target);
 });
+
 
 allbtn.addEventListener('click', ()=> allTab());
 
---
+++ src/tabSwitching.js
@@ -0,0 +1,20 @@
+export function highlightBG(targetLi){
+    const legendOptions = document.getElementById('projects')
+    const projectsOptions = document.getElementById('lists');
+    let items = legendOptions.querySelectorAll('li');
+    const projOpt = projectsOptions.querySelectorAll("li");
+    
+    // remove background from all tabs
+    projOpt.forEach(opt => {
+        opt.style.backgroundColor = "";
+        opt.style.borderRadius = "";
+    });
+
+    items.forEach(item => {
+        item.style.backgroundColor = "";
+        item.style.borderRadius = "";
+    });
+
+    targetLi.style.backgroundColor = ' rgba(74, 6, 119, 0.4)';
+    targetLi.style.borderRadius = '1.3rem';
+}
